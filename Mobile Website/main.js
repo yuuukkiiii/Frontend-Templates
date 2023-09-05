9fb06c0c48458f2619a5497e3287f492fda3cff3
mohamed <alxmohamedemadeldin@gmail.com>
2023-09-05 19:02:43 +0300

    Updated

---
+++ Mobile Website/main.js
@@ -0,0 +1,9 @@
+const phonePicture = document.querySelector('.phone-picture');
+
+const productImages = document.querySelectorAll('.product-menu img');
+
+productImages.forEach(img => {
+    img.addEventListener('mouseenter', function() {
+        phonePicture.src = this.src;
+    });
+});
